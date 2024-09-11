4fec69d32a51fd3b46dbb336c15d52dfa7b26aa5
Arete23 <aretes@bk.ru>
2024-09-12 01:47:09 +0300

    12.09

---
+++ Переменные.py
@@ -0,0 +1,7 @@
+first_count = 12
+second_count = 1.5
+name = 'Python'
+name_time = second_count/first_count
+print('Курс:', name, ', всего задач:', first_count, ', затрачено часов:', second_count, ', среднее время выполнения',name_time, 'часа')
+
+
