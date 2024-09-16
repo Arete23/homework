6a2be0d68d5c8ac2fa10204a0d4ad6efa5571ecd
Arete23 <aretes@bk.ru>
2024-09-16 13:18:29 +0300

    Add files via upload

---
+++ avg_mark.py
@@ -0,0 +1,19 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+avg_grades = (
+    sum(grades[0]) / len(grades[0]),
+    sum(grades[1]) / len(grades[1]),
+    sum(grades[2]) / len(grades[2]),
+    sum(grades[3]) / len(grades[3]),
+    sum(grades[4]) / len(grades[4])
+)
+
+students_list = list(sorted(students))
+grades_list = list(avg_grades)
+dict = {}
+
+for i in range(len(grades_list)):
+    dict[students_list[i]] = grades_list[i]
+
+print(dict)
