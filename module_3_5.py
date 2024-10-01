c39adc7e7e2c67238d7c0ae8f536ccc09b2c0e2e
Arete23 <aretes@bk.ru>
2024-10-01 14:44:00 +0300

    Add files via upload

--- module_3_5.py
+++ module_3_5.py
@@ -1,5 +1,6 @@
 def get_multiplied_digits(number):
     str_number = str(number)
+    str_number = str_number.replace('0', '')
     first = int(str_number[0])
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
@@ -9,3 +10,7 @@ def get_multiplied_digits(number):
 
 result = get_multiplied_digits(40203)
 print(result)
+result = get_multiplied_digits(40220)
+print(result)
+# result = get_multiplied_digits(0123)
+# print(result)
