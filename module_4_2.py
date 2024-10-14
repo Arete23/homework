289fd8a5d0d7150f9b7210083202a4401f2c30ae
Arete23 <aretes@bk.ru>
2024-10-14 17:09:41 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,9 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+    inner_function()
+
+# inner_function() # ошбика, т.к. невозможо доставать значения внутри функции (извне)
+
+test_function()
