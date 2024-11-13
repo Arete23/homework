828324198dd7fbc069ecec4715d422c720a252d6
Arete23 <aretes@bk.ru>
2024-11-13 21:41:37 +0300

    Add files via upload

--- module_8_1.py
+++ module_8_1.py
@@ -1,16 +1,9 @@
 def add_everything_up(a, b):
     try:
-        if isinstance(a, (int, float)) and isinstance(b, (int, float)):
-            return round(a + b, 3)
+       return round(a + b, 3)
 
-        elif isinstance(a, str) and isinstance(b, str):
-            return a + b
-
-        else:
-            raise TypeError
-        
     except TypeError:
-        return f'{str(a)}{str(b)}'
+        return str(a) + str(b)
 
 
 print(add_everything_up(123.456, 'строка'))
