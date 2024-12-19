1c3dd4666c03463328d3930a1e156b73360e9d68
Arete23 <aretes@bk.ru>
2024-12-19 08:47:51 +0300

    Add files via upload

--- Intros.py
+++ Intros.py
@@ -3,13 +3,10 @@ def introspection_info(obj):
     attributes = [at for at in dir(obj) if not callable(getattr(obj, at))]
     methods = [m for m in dir(obj) if callable(getattr(obj, m))]
     module = obj.__class__.__module__
-    dict_int = {
-        'type': obj_type,
+    dict_int = {'type': obj_type,
         'attributes': attributes,
         'methods': methods,
-        'module': module,
-
-    }
+        'module': module,}
     return dict_int
 
 
