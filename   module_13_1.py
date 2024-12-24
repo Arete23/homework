af19aff970ceaa4cb837e66c03f5543e25fa1192
Arete23 <aretes@bk.ru>
2024-12-24 16:36:12 +0300

    Add files via upload

---   module_13_1.py
+++   module_13_1.py
@@ -3,9 +3,8 @@ import asyncio
 async def start_strongman(name, power):
     print(f'Силач {name} начал соревнование')
     for i in range(5):
-        nbol = i+1
-        await asyncio.sleep(nbol//power)
-        print(f'Силач {name} поднял {nbol} шар')
+        await asyncio.sleep(1 / power)
+        print(f'Силач {name} поднял {i + 1} шар')
     print(f'Силач {name} закончил соревнования')
 
 async def start_tournament():
